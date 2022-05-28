6c8399aef49af9ae35af3c1903afeb8fcc85cca4
Victor Gallegos <316160456@ciencias.unam.mx>
2022-05-27 23:43:13 -0500

    Agregando colores limpiar texto, guardar y diseño

--- app.js
+++ app.js
@@ -4,16 +4,91 @@ $(document).ready(function () {
 
   $('.color-box').click(function () {
     var color = $(this).css('background-color');
-    $('notepad').css('background-color', color);
-    $('#title-field').css('background-color', color);
-    $('#body-field').css('background-color', color);
+    // $('notepad').css('background-color', color);
+    $('#title-field').css('color', color);
+    // $('#body-field').css('color', color);
+  });
+
+  $('.color-boxbody').click(function () {
+    var colorbody = $(this).css('background-color');
+    // $('notepad').css('background-color', color);
+    // $('#title-field').css('color', color);
+    $('#body-field').css('color', colorbody);
   });
 
   $('#btn-save').click(function () {
     var title = $('#title-field').val();
     var body = $('#body-field').val();
     if (title === '' && body === '') {
-      alert('Please add a title or body to your note.');
+      alert('Agregue un título o cuerpo a su nota.');
+      return;
+    }
+    var created = new Date();
+    var color = $('notepad').css('background-color');
+    var id = noteCount + 1;
+    if (activeNote) {
+      $('#' + activeNote)[0].children[0].innerHTML = title;
+      $('#' + activeNote)[0].children[1].innerHTML = created.toLocaleString('en-ES');
+      $('#' + activeNote)[0].children[2].innerHTML = body;
+      $('#' + activeNote)[0].style.backgroundColor = color;
+      activeNote = null;
+      $('#edit-mode').removeClass('display').addClass('no-display');
+    } else {
+      var created = new Date();
+      $('#listed').append(
+        '<div id="note' +
+          id +
+          '" style="background-color: ' +
+          color +
+          '"><div class="list-title">' +
+          title +
+          '</div> <div class="list-date">' +
+          created.toLocaleString('en-ES') +
+          '</div> <div class="list-text">' +
+          body +
+          '</div> </div>'
+      );
+      noteCount++;
+    }
+    $('#title-field').val('');
+    $('#body-field').val('');
+    $('notepad').css('background-color', '#41a0e8');
+    $('#title-field').css('background-color', 'white');
+    $('#body-field').css('background-color', '#white');
+  });
+
+  $('#btn-delete').click(function () {
+    if (activeNote) {
+      $('#' + activeNote)[0].remove();
+      activeNote = null;
+      $('#edit-mode').removeClass('display').addClass('no-display');
+    }
+    $('#title-field').val('');
+    $('#body-field').val('');
+    $('notepad').css('background-color', '#41a0e8');
+    $('#title-field').css('background-color', 'white');
+    $('#body-field').css('background-color', 'white');
+  });
+
+  $('#listed').click(function (e) {
+    var id = e.target.parentElement.id;
+    var color = e.target.parentElement.style.backgroundColor;
+    activeNote = id;
+    $('#edit-mode').removeClass('no-display').addClass('display');
+    var titleSel = $('#' + id)[0].children[0].innerHTML;
+    var bodySel = $('#' + id)[0].children[2].innerHTML;
+    $('#title-field').val(titleSel);
+    $('#body-field').val(bodySel);
+    $('notepad').css('background-color', color);
+    $('#title-field').css('background-color', white);
+    $('#body-field').css('background-color', white);
+  });
+
+  $('#btn-nuevo').click(function () {
+    var title = $('#title-field').val();
+    var body = $('#body-field').val();
+    if (title === '' && body === '') {
+      alert('Agregue un título o cuerpo a su nota.');
       return;
     }
     var created = new Date();
@@ -45,35 +120,8 @@ $(document).ready(function () {
     }
     $('#title-field').val('');
     $('#body-field').val('');
-    $('notepad').css('background-color', 'white');
+    $('notepad').css('background-color', '#41a0e8');
     $('#title-field').css('background-color', 'white');
     $('#body-field').css('background-color', 'white');
-  });
-
-  $('#btn-delete').click(function () {
-    if (activeNote) {
-      $('#' + activeNote)[0].remove();
-      activeNote = null;
-      $('#edit-mode').removeClass('display').addClass('no-display');
-    }
-    $('#title-field').val('');
-    $('#body-field').val('');
-    $('notepad').css('background-color', 'white');
-    $('#title-field').css('background-color', 'white');
-    $('#body-field').css('background-color', 'white');
-  });
-
-  $('#listed').click(function (e) {
-    var id = e.target.parentElement.id;
-    var color = e.target.parentElement.style.backgroundColor;
-    activeNote = id;
-    $('#edit-mode').removeClass('no-display').addClass('display');
-    var titleSel = $('#' + id)[0].children[0].innerHTML;
-    var bodySel = $('#' + id)[0].children[2].innerHTML;
-    $('#title-field').val(titleSel);
-    $('#body-field').val(bodySel);
-    $('notepad').css('background-color', color);
-    $('#title-field').css('background-color', color);
-    $('#body-field').css('background-color', color);
   });
 });
